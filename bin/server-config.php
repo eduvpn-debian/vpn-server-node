c4bf5a96cb6d68d18fb4c6387920156c002325cb
Gijs Molenaar <gijs@pythonic.nl>
2017-10-25 17:01:19 +0200

    New upstream version 1.0.4

--- bin/server-config.php
+++ bin/server-config.php
@@ -7,8 +7,15 @@
  * Copyright: 2016-2017, The Commons Conservancy eduVPN Programme
  * SPDX-License-Identifier: AGPL-3.0+
  */
-require_once sprintf('%s/vendor/autoload.php', dirname(__DIR__));
+$baseDir = dirname(__DIR__);
 
+// find the autoloader (package installs, composer)
+foreach (['src', 'vendor'] as $autoloadDir) {
+    if (@file_exists(sprintf('%s/%s/autoload.php', $baseDir, $autoloadDir))) {
+        require_once sprintf('%s/%s/autoload.php', $baseDir, $autoloadDir);
+        break;
+    }
+}
 use SURFnet\VPN\Common\CliParser;
 use SURFnet\VPN\Common\Config;
 use SURFnet\VPN\Common\HttpClient\CurlHttpClient;
@@ -21,7 +28,6 @@ try {
         'Generate VPN server configuration for an instance',
         [
             'instance' => ['the VPN instance', true, false],
-            'profile' => ['the profile identifier', true, true],
             'generate' => ['generate a new certificate for the server', false, false],
         ]
     );
@@ -33,19 +39,15 @@ try {
     }
 
     $instanceId = $opt->hasItem('instance') ? $opt->getItem('instance') : 'default';
-
-    $profileId = $opt->getItem('profile');
     $generateCerts = $opt->hasItem('generate');
 
-    $configFile = sprintf('%s/config/%s/config.php', dirname(__DIR__), $instanceId);
+    $configFile = sprintf('%s/config/%s/config.php', $baseDir, $instanceId);
     $config = Config::fromFile($configFile);
 
     $vpnUser = $config->hasItem('vpnUser') ? $config->getItem('vpnUser') : 'openvpn';
     $vpnGroup = $config->hasItem('vpnGroup') ? $config->getItem('vpnGroup') : 'openvpn';
 
-    $vpnConfigDir = sprintf('%s/openvpn-config', dirname(__DIR__));
-    $vpnTlsDir = sprintf('%s/openvpn-config/tls/%s/%s', dirname(__DIR__), $instanceId, $profileId);
-
+    $vpnConfigDir = sprintf('%s/openvpn-config', $baseDir);
     $serverClient = new ServerClient(
         new CurlHttpClient([$config->getItem('apiUser'), $config->getItem('apiPass')]),
         $config->getItem('apiUri')
@@ -53,20 +55,24 @@ try {
 
     $instanceNumber = $serverClient->get('instance_number');
     $profileList = $serverClient->get('profile_list');
-    $profileConfigData = $profileList[$profileId];
 
-    $profileConfigData['_user'] = $vpnUser;
-    $profileConfigData['_group'] = $vpnGroup;
-    $profileConfig = new ProfileConfig($profileConfigData);
+    $profileIdList = array_keys($profileList);
+    foreach ($profileIdList as $profileId) {
+        $vpnTlsDir = sprintf('%s/openvpn-config/tls/%s/%s', $baseDir, $instanceId, $profileId);
+        $profileConfigData = $profileList[$profileId];
+        $profileConfigData['_user'] = $vpnUser;
+        $profileConfigData['_group'] = $vpnGroup;
+        $profileConfig = new ProfileConfig($profileConfigData);
 
-    $o = new OpenVpn($vpnConfigDir, $vpnTlsDir);
-    $o->writeProfile($instanceNumber, $instanceId, $profileId, $profileConfig);
-    if ($generateCerts) {
-        // generate a CN based on date and profile, instance
-        $dateTime = new DateTime('now', new DateTimeZone('UTC'));
-        $dateString = $dateTime->format('YmdHis');
-        $cn = sprintf('%s.%s.%s', $dateString, $profileId, $instanceId);
-        $o->generateKeys($serverClient, $cn);
+        $o = new OpenVpn($vpnConfigDir, $vpnTlsDir);
+        $o->writeProfile($instanceNumber, $instanceId, $profileId, $profileConfig);
+        if ($generateCerts) {
+            // generate a CN based on date and profile, instance
+            $dateTime = new DateTime('now', new DateTimeZone('UTC'));
+            $dateString = $dateTime->format('YmdHis');
+            $cn = sprintf('%s.%s.%s', $dateString, $profileId, $instanceId);
+            $o->generateKeys($serverClient, $cn);
+        }
     }
 } catch (Exception $e) {
     echo sprintf('ERROR: %s', $e->getMessage()).PHP_EOL;
